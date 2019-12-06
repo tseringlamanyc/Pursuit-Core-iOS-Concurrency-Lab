8f30cbe7eb9add0f379dcd7746d8bf8f65094fc8
Tsering Lama <tseringlama@pursuit.org>
2019-12-06 16:12:33 -0500

    detail view controller for country done

--- ConcurrencyLab/ConcurrencyLab/DetailViewController.swift
+++ ConcurrencyLab/ConcurrencyLab/DetailViewController.swift
@@ -9,10 +9,34 @@
 import UIKit
 
 class DetailViewController: UIViewController {
+    
+    @IBOutlet weak var flagImage: UIImageView!
+    @IBOutlet weak var nameText: UILabel!
+    @IBOutlet weak var capitalText: UILabel!
+    @IBOutlet weak var populationText: UILabel!
+    
+    var country: Country!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        loadData()
+    }
+    
+    func loadData() {
+        FlagClient.getImage(countryUrl: "https://www.countryflags.io/\(country.alpha2Code)/flat/64.png") { [unowned self] (result) in
+        switch result {
+        case .failure(let error):
+            print("\(error)")
+        case .success(let image):
+            DispatchQueue.main.async {
+                self.flagImage.image = image
+            }
+        }
     }
 
+        nameText.text = country.name
+        capitalText.text = country.capital
+        populationText.text = country.population.description
 
+}
 }
